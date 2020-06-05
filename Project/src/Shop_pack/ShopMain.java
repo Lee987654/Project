c42d8e44f21737a845974870e08f8882991fd303
admin <ok2qkq@naver.com>
2020-06-05 15:28:10 +0900

    06-05

--- Project/src/Shop_pack/ShopMain.java
+++ Project/src/Shop_pack/ShopMain.java
@@ -1,5 +1,23 @@
 package Shop_pack;
 
-public class ShopMain {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class ShopMain extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("Shop.fxml"));
+		Scene scene = new Scene(root);
+		
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+	}
+public static void main(String[] args) {
+	launch(args);
+}
 }
